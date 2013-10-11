066f52b073add1160db81db788f5c24542d4b863
Lam <newbournlam@gmail.com>
2013-10-11 18:47:38 +0800

    2

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,49 @@
-$(function(){
-	var container_h = $(window).height() - 31;
-	var h = container_h/2 - 1;
+var metro = {
+    render: function(delay){
+        var pos = metro.getPos($(window).height() - 30, 300),
+            size = pos.size,
+            row = pos.row;
 
-	console.log(container_h)
-	$('.metro li').each(function(i, v){
-		$(v).css({height: h + 'px', webkitTransform: 'translate3d(0, '+ i*h +'px, 0)'});
-	});
+        $('.metro li').each(function(i, v){
+            var x = Math.floor(i/row),
+                y = i%row;
+                if(delay){
+                    metro.show(v, size, x, y);
+                }else{
+                    console.log(i)
+                    setTimeout(function(){
+                         metro.show(v, size, x, y);
+                    }, 100 * i);    
+                }
+        });
+    },
+
+    getPos: function(total, height){
+        var row = Math.round(total / height);
+        return {
+            size: Math.floor(total / row),
+            row: row
+        }
+    },
+
+    show: function(obj, size, x, y){
+        $(obj).css({
+            width: size +'px', 
+            height: size + 'px', 
+            transform: 'translate3d('+ (parseInt(x*size)+x) +'px, '+ (parseInt(y*size)+y) +'px, 0)',
+            opacity: '1'
+        });   
+    }
+
+}
+$(function(){
+    $('.content').css({width: $(window).width() +'px'});
+
+    //可用高度
+    metro.render();
+
+    $(window).on('resize', function(){
+        metro.render(true);
+    });
+    
 });
